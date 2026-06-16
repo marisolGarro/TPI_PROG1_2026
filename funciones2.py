cd969342a5ea466bcd93c1079bdcf162ef574165
marisolGarro <141685137+marisolGarro@users.noreply.github.com>
2026-06-16 18:49:06 -0300

    función de filtrar país por continente y try en el main

--- funciones2.py
+++ funciones2.py
@@ -11,28 +11,35 @@ def normalizar(texto):
 
 def buscar_pais():
     pais_buscado=normalizar(input("Ingrese el país que desea buscar: "))
-    try:
-        with open("paises.csv","r",newline="",encoding="utf-8-sig") as archivo:
-            #Cada fila la representa como un diccionario
-            lector=csv.DictReader(archivo)
-            encontrado=False
-            for fila in lector:
-                #verifico sila cadena de caracteres ingresado en pais_buscado esta contenido en el nombre del país
-                if pais_buscado in normalizar(fila["nombre"].lower()):
-                    print(f"Pais: {fila["nombre"]}")
-                    print(f"Población: {fila["poblacion"]}")
-                    print(f"Superficie: {fila["superficie"]}")
-                    print(f"Contienente: {fila["continente"]}\n")
-                    encontrado=True
-            if not encontrado:
-                raise ValueError("El país ingresado no existe en el registro")
-    except FileNotFoundError:
-        print("Error: No se encontró el archivo paises.csv")
-    except KeyError:
-        print("Error: el archivo no contiene las columnas esperadas")
-    except PermissionError:
-        print("Error: El archivo esta siendo utilizado en otro programa")
-    except ValueError as e:
-        print(f"Error: {e}")
-    except Exception as e:
-        print(f"Error: ocurrió un error inesperado {e}")
+    
+    with open("paises.csv","r",newline="",encoding="utf-8-sig") as archivo:
+        #Cada fila la representa como un diccionario
+        lector=csv.DictReader(archivo)
+        encontrado=False
+        for fila in lector:
+            #verifico sila cadena de caracteres ingresado en pais_buscado esta contenido en el nombre del país
+            if pais_buscado in normalizar(fila["nombre"]):
+                print(f"Pais: {fila["nombre"]}")
+                print(f"Población: {fila["poblacion"]}")
+                print(f"Superficie: {fila["superficie"]}")
+                print(f"Contienente: {fila["continente"]}\n")
+                encontrado=True
+        if not encontrado:
+            raise ValueError("El país ingresado no existe en el registro")
+    
+
+def filtrar_pais_continente():
+    buscar_continente=normalizar(input(f"Ingrese el continente: "))
+    with open("paises.csv","r",newline="",encoding="utf-8-sig") as archivo:
+        lector=csv.DictReader(archivo)
+        encontrado=False
+        for lista in lector:
+            if buscar_continente==normalizar(lista["continente"]):
+                print(f"\nPais: {lista["nombre"]}")
+                print(f"Población: {lista["poblacion"]}")
+                print(f"Superficie: {lista["superficie"]}")
+                
+                encontrado=True
+        if not encontrado:
+            raise ValueError("El país ingresado no existe en el registro")
+            
--- main.py
+++ main.py
@@ -6,66 +6,76 @@ escribir_csv(paises)
 while True:
     mostrar_menu()
     opcion=input("Ingrese una opción: ")
-
-    match(opcion):
-        case "1":
-            pass
-        case "2":
-            pass
-        case "3":
-            buscar_pais()
-        case "4":
-            while True:
-                filtrar_menu()
-                opcion=input("Ingrese una opcion: ")
-                match(opcion):
-                    case "1":
-                        pass
-                    case "2":
-                        pass
-                    case "3":
-                        pass
-                    case "4":
-                        print("Volviendo al menú principal")
-                        break
-                    case _:
-                        print("La opción ingresada es incorrecta")
-        case "5":
-            while True:
-                ordenar_menu()
-                opcion=input("Ingrese una opcion: ")
-                match(opcion):
-                    case "1":
-                        pass
-                    case "2":
-                        pass
-                    case "3":
-                        pass
-                    case "4":
-                        print("Volviendo al menú principal")
-                        break
-                    case _:
-                        print("La opción ingresada es incorrecta")
-        case "6":
-            while True:
-                estadistica_menu()
-                opcion=input("Ingrese una opcion: ")
-                match(opcion):
-                    case "1":
-                        pass
-                    case "2":
-                        pass
-                    case "3":
-                        pass
-                    case "4":
-                        pass
-                    case "5":
-                        print("Volviendo al menú principal")
-                        break
-                    case _:
-                        print("La opción ingresada es incorrecta")
-        case "7":
-            print("Hasta luego!")
-            break
-        case _:
-            print("La opción ingresada es incorrecta")
+    try:
+        match(opcion):
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                buscar_pais()
+            case "4":
+                while True:
+                    filtrar_menu()
+                    opcion=input("Ingrese una opcion: ")
+                    match(opcion):
+                        case "1":
+                            filtrar_pais_continente()
+                        case "2":
+                            pass
+                        case "3":
+                            pass
+                        case "4":
+                            print("Volviendo al menú principal")
+                            break
+                        case _:
+                            print("La opción ingresada es incorrecta")
+            case "5":
+                while True:
+                    ordenar_menu()
+                    opcion=input("Ingrese una opcion: ")
+                    match(opcion):
+                        case "1":
+                            pass
+                        case "2":
+                            pass
+                        case "3":
+                            pass
+                        case "4":
+                            print("Volviendo al menú principal")
+                            break
+                        case _:
+                            print("La opción ingresada es incorrecta")
+            case "6":
+                while True:
+                    estadistica_menu()
+                    opcion=input("Ingrese una opcion: ")
+                    match(opcion):
+                        case "1":
+                            pass
+                        case "2":
+                            pass
+                        case "3":
+                            pass
+                        case "4":
+                            pass
+                        case "5":
+                            print("Volviendo al menú principal")
+                            break
+                        case _:
+                            print("La opción ingresada es incorrecta")
+            case "7":
+                print("Hasta luego!")
+                break
+            case _:
+                print("La opción ingresada es incorrecta")
+    except FileNotFoundError:
+        print("Error: No se encontró el archivo paises.csv")
+    except KeyError:
+        print("Error: el archivo no contiene las columnas esperadas")
+    except PermissionError:
+        print("Error: El archivo esta siendo utilizado en otro programa")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except Exception as e:
+        print(f"Error: ocurrió un error inesperado {e}")
